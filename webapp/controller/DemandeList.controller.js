c13a4c285a32ae33d592ec927de0ff982b3f52b2
Khemiriasma <akhemiri@veoworldwide.com>
2023-06-05 09:02:44 +0000

    commit 05/06

--- webapp/controller/DemandeList.controller.js
+++ webapp/controller/DemandeList.controller.js
@@ -9,6 +9,11 @@ sap.ui.define([
 
         return Controller.extend("comep.comep.controller.DemandeList", {
             onInit: function () {
+                this.oModel = this.getOwnerComponent().getModel("ZWD_TEST_SRV");
+                //this.getView().setModel(i18nModel, "i18n");
+                this.oTable = this.getView().byId("idTable");
+                this.getView().setModel(this.oModel);
+                this.oTable.setModel(this.oModel);
 
             }
         });
