227afdbf9045f66cd3b69e1429509b48b477d673
Khemiriasma <akhemiri@veoworldwide.com>
2023-06-15 08:17:38 +0000

    commit 15/06

--- webapp/controller/DemandeList.controller.js
+++ webapp/controller/DemandeList.controller.js
@@ -1,27 +1,31 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/resource/ResourceModel",
-    "sap/ui/Device"
+    "sap/ui/Device",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "comep/comep/model/formatter"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, ResourceModel, Device) {
+    function (Controller, ResourceModel, Device, Filter, FilterOperator,formatter) {
         "use strict";
 
 
         return Controller.extend("comep.comep.controller.DemandeList", {
+            formatter: formatter,
             onInit: function () {
 
-                var i18nModel = new ResourceModel({
-                    bundleName: "comep.comep.i18n.i18n"
-                });
+
                 this.oRouter = this.getOwnerComponent().getRouter();
-                this.oModel = this.getOwnerComponent().getModel("ZWD_TEST_SRV");
-                this.getView().setModel(i18nModel, "i18n");
+                this.oModel = this.getOwnerComponent().getModel("ZCOMEP_POC_SRV");
+                this.oBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
                 this.oTable = this.getView().byId("idTable");
+                this.oView = this.getView();
                 this.getView().setModel(this.oModel);
                 this.oTable.setModel(this.oModel);
+                this.Filters = [];
             },
             onDemandePress: function (oEvent) {
 
@@ -49,15 +53,175 @@ sap.ui.define([
 
                 this.oRouter.navTo("DemandeDetail", {
 
-                    dem_descr: oItem.getBindingContext().getProperty("dem_descr")
+                    DemCode: oItem.getBindingContext().getProperty("DemCode")
                 }, bReplace);
 
 
 
                 // }
 
-            }
+            },
+            _createDialog: function (sDialog) {
+                var oDialog = sap.ui.xmlfragment(sDialog, this);
+                jQuery.sap.syncStyleClass("sapUiSizeCompact", this._oView, oDialog);
 
+                this.getView().addDependent(oDialog);
+                return oDialog;
+            },
+            onDemandeValueHelp: function (oController) {
+
+                if (this._valueHelpDialogDemande) {
+
+                    var aTokens = oController.getSource().getTokens();
+                    var oSelectedItems = this._valueHelpDialogDemande._oSelectedItems;
+                    this._valueHelpDialogDemande.destroy();
+                }
+                this._valueHelpDialogDemande = this._createDialog("comep.comep.view.fragments.DemandeDialog");
+
+                var oColModel = new sap.ui.model.json.JSONModel();
+                oColModel.setData({
+                    cols: [{
+                        label: this.oBundle.getText("demande"),
+                        template: "DemCode"
+                    }, {
+                        label: this.oBundle.getText("description"),
+                        template: "DemDescr"
+                    }]
+                });
+
+                var oTable = this._valueHelpDialogDemande.getTable();
+                oTable.setModel(oColModel, "columns");
+                var oInput = this.getView().byId("demandeInputId");
+                var oModel = this.getOwnerComponent().getModel("ZCOMEP_POC_SRV");
+                oModel.read("/DemandeSet", {
+                    success: function (oData) {
+                        var oModelRows = new sap.ui.model.json.JSONModel();
+                        oModelRows.setData({
+                            data: oData.results
+                        });
+                        oTable.setModel(oModelRows);
+                        oTable.bindRows("/data");
+
+                    },
+                    error: function (oError) {
+
+                    }
+
+                });
+                this._valueHelpDialogDemande.open();
+
+            },
+            onConfirm: function (oEvent) {
+
+                var inputValue, filterValue, keyValue,
+                    oInput;
+
+                switch (oEvent.getParameter("id")) {
+                    case "demandeValueHelp":
+                        inputValue = "demandeInputId";
+                        keyValue = "Demande";
+                        filterValue = "DemCode";
+                        break;
+
+                }
+                oInput = this.getView().byId(inputValue);
+                var aTokens = oEvent.getParameter("tokens");
+                oInput.setTokens(aTokens);
+                oEvent.getSource().close();
+
+            },
+            onCancel: function (oEvent) {
+
+                oEvent.getSource().close();
+            },
+
+            onFilterChange: function () {
+                var data = 'onFilterChange';
+            },
+            onChange: function (oEvent) {
+                var oSourceId;
+                if (oEvent.getId() === 'tokenUpdate' && oEvent.getParameter("type") === 'removed') {
+                    oSourceId = oEvent.getParameter("id");
+
+                    if (oSourceId.includes('demandeInputId')) {
+                        var skey = oEvent.getParameter("removedTokens")[0].getProperty("key");
+                        var sName = oEvent.getParameter("removedTokens")[0].getProperty("text");
+                        var aTokens = this.oView.byId('demandeInputId').getTokens();
+                        for (var i = 0; i < aTokens.length; i++) {
+                            if ((this.Filters[i].oValue1 === skey)) {
+                                // remove filter from filters list
+                                this.Filters.splice(i, 1);
+                                aTokens.splice(i, 1);
+                                i--;
+                            }
+                        }
+                    }
+
+                }
+                var oTemplate = this._createTemplate();
+                this.oTable.bindItems({
+                    path: "/DemandeSet",
+                    template: oTemplate,
+                    filters: this.Filters,
+                });
+
+
+
+            },
+            onSearch: function () {
+                this.oTable.setBusy(true);
+
+                this._getInputFilters("demandeInputId", 'DemCode');
+
+                var oTemplate = this._createTemplate();
+                this.oTable.bindItems({
+                    path: "/DemandeSet",
+                    template: oTemplate,
+                    filters: this.Filters,
+                });
+                this.oTable.setBusy(false);
+            },
+            _getInputFilters: function (sInput, sFilterName) {
+
+                var oInput = this.getView().byId(sInput);
+                var aTokens = oInput.getTokens();
+                if (aTokens.length !== 0) {
+                    for (var j = 0; j < aTokens.length; j++) {
+                        var oFilterItem = new sap.ui.model.Filter(sFilterName, sap.ui.model.FilterOperator.EQ, aTokens[j].getKey());
+                        this.Filters.push(oFilterItem);
+                    }
+
+                }
+            },
+            _createTemplate: function () {
+
+                return new sap.m.ColumnListItem({
+                    type: "Navigation",
+                    press: (this.onDemandePress).bind(this),
+
+                    cells: [
+                        new sap.m.ObjectIdentifier({
+                            title: "{DemCode}"
+                        }),
+                        new sap.m.ObjectAttribute({
+                            text: "{DemDescr}"
+
+                        }),
+                        new sap.m.ObjectAttribute({
+                            text: "{DemStatut}"
+
+                        }),
+                        new sap.m.ObjectAttribute({
+                            text: "{DemDateCrea}"
+
+                        }),
+                        new sap.m.ObjectAttribute({
+                            text: "{DemResponsable}"
+
+                        })
+                    ]
+                });
+            }
 
         });
     });
